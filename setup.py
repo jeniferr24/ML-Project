16b155ac5485d4506bde69c603240cc0a104ded4
Vidya Swarupa <vidya03swarupa@gmail.com>
2023-08-29 14:43:46 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+HYPEN_E_DOT='-e .'
+def get_requirements(files_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    
+    requirements=[]
+    with open(files_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n"," ")for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+            
+    return requirements 
+               
+    
+
+
+
+setup(
+name='ML-PROJECT',
+version='0.0.1',
+author='vidya',
+author_email='vidya03swarupa@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
